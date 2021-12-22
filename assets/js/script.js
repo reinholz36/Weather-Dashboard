f12fd1cd93f85e66b39dd3a840d21e4b834bc88a
reinholz36 <dreinholz28@gmail.com>
2021-12-21 19:58:52 -0500

    added city name display and time conversion from unix to standard

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,13 @@ var searchFormEl = document.querySelector("#searchcityform");
 var searchCityInputEl = document.querySelector("#searchcity");
 var searchHistoryCityEl = document.querySelector("#history-container");
 var citySearchTermEl = document.querySelector("#city-search-term");
+var cityTitleCurrentEl = document.querySelector("#weather-in");
+var cityDetailCurrentEl = document.querySelector("#city-current-details");
+var tmpCurrentEl = document.querySelector("#tmp-current");
+var wndCurrentEl = document.querySelector("#wnd-current");
+var humidCurrentEl = document.querySelector("#humid-current");
+var uvCurrentEl = document.querySelector("#uv-current");
+var cityHistoryArray = [];
 
 
 var formSubmitHandler = function(event) {
@@ -9,13 +16,12 @@ var formSubmitHandler = function(event) {
     var cityname = searchCityInputEl.value.trim();
 
     if (cityname) {
-        //Update this function name(getUserRepos) example only! 
         getCityWeather(cityname);
+        saveCityName(cityname);
         searchCityInputEl.value = "";
     } else {
         alert("Please enter a City name");
     }
-    console.log(event);
 };
 
 searchFormEl.addEventListener("submit", formSubmitHandler);
@@ -23,13 +29,53 @@ searchFormEl.addEventListener("submit", formSubmitHandler);
 // pulls weather data based on city submitted
 var getCityWeather = function (cityname) {
     //format the github api url
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=4bba3cfced2651343b44d79c2548661a";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&units=imperial&date=MM/dd/yyyy&appid=4bba3cfced2651343b44d79c2548661a";
        
     // make a request to the url
     fetch(apiUrl).then(function(response) {
+        if (response.ok) {
         response.json().then(function(data) {
-            console.log(data);
+            console.log("data", data);
+            // unixConverter(data);
+            displayCurrent(data);
         });
+        } else {
+            alert('Error: City Not Found');
+        }
     });
 };
 
+// *NOT Finished* Save city name to local storage 
+var saveCityName = function (cityname) {
+    console.log("city name", cityname);
+}
+
+//Convert time from UNIX to month/day/year
+var unixConverter = function(data) {
+    var unixTime = data.dt;
+    console.log("unixTime", unixTime);
+    var date = new Date(unixTime * 1000)
+    var convertedTime = (date.getMonth()+1)
+    + "/"+
+    +date.getDate()
+    + "/"
+    +date.getFullYear();
+    return convertedTime;
+}
+
+// Display current city and weather
+var displayCurrent = function(data) {
+    if (data.length === 0) {
+        citySearchTermEl.textContent = "No City found.";
+        return;
+    }
+    console.log("data2", data);
+    citySearchTermEl.textContent = data.name 
+    +" "
+    + unixConverter(data)
+    +" "
+    + data.weather.icon;
+
+    cityTitleCurrentEl.appendChild(citySearchTermEl);
+
+}
