64870621ce907f9f2987c0d4ec22de3259b262fe
reinholz36 <dreinholz28@gmail.com>
2021-12-27 15:38:50 -0500

    added dynamic five day forcast based on city selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var searchHistoryCityEl = document.querySelector("#history-container");
 var citySearchTermEl = document.querySelector("#city-search-term");
 var cityTitleCurrentEl = document.querySelector("#weather-in");
 var cityDetailCurrentEl = document.querySelector("#city-current-details");
+var cityIconCurrentEl = document.querySelector("#current-icon");
 var tmpCurrentEl = document.querySelector("#tmp-current");
 var tmpDisplayCurrentEl = document.querySelector("#tmp-display-current");
 var wndCurrentEl = document.querySelector("#wnd-current");
@@ -29,6 +30,11 @@ var dayTwoIconEl = document.querySelector("#day-two-icon");
 var dayThreeIconEl = document.querySelector("#day-three-icon");
 var dayFourIconEl = document.querySelector("#day-four-icon");
 var dayFiveIconEl = document.querySelector("#day-five-icon");
+var dayOneIconDisplayEl = document.querySelector("#day-one-icon-display");
+var dayTwoIconDisplayEl = document.querySelector("#day-two-icon-display");
+var dayThreeIconDisplayEl = document.querySelector("#day-three-icon-display");
+var dayFourIconDisplayEl = document.querySelector("#day-four-icon-display");
+var dayFiveIconDisplayEl = document.querySelector("#day-five-icon-display");
 var dayOneTmpEl = document.querySelector("#day-one-tmp");
 var dayTwoTmpEl = document.querySelector("#day-two-tmp");
 var dayThreeTmpEl = document.querySelector("#day-three-tmp");
@@ -162,17 +168,16 @@ var displayCurrent = function(data) {
         citySearchTermEl.textContent = "No City found.";
         return;
     }
-    var iconEl = document.createElement("img")
-    iconEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
+    cityIconCurrentEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
     citySearchTermEl.textContent = data.name 
     +" "
     + unixConverter(data)
     +" "
     cityTitleCurrentEl.appendChild(citySearchTermEl);
-    cityTitleCurrentEl.appendChild(iconEl);
+    cityTitleCurrentEl.appendChild(cityIconCurrentEl);
 
     tmpDisplayCurrentEl.textContent = " " + Math.round(data.main.temp) + " ℉"
-    wndDisplayCurrentEl.textContent = " " + data.wind.speed + " MPH"
+    wndDisplayCurrentEl.textContent = " " + Math.round(data.wind.speed) + " MPH"
     humidDisplayCurrentEl.textContent = " " + data.main.humidity + " %"
     
     tmpCurrentEl.appendChild(tmpDisplayCurrentEl);
@@ -180,12 +185,42 @@ var displayCurrent = function(data) {
     humidCurrentEl.appendChild(humidDisplayCurrentEl);
 }
 
-// Display 5 day forcast from array 7, 14, 21, 28, 35
+// Display 5 day forcast
 var displayFiveDay = function(data) {
     if (data.length === 0) {
         fiveDayTitleErrorEl.textContent = "No Five Day Forcast found.";
         return;
     }
+
+    dayOneDateEl.textContent = moment.unix(data.list[7].dt).format("ddd MM/DD/YYYY")
+    dayTwoDateEl.textContent = moment.unix(data.list[14].dt).format("ddd MM/DD/YYYY")
+    dayThreeDateEl.textContent = moment.unix(data.list[21].dt).format("ddd MM/DD/YYYY")
+    dayFourDateEl.textContent = moment.unix(data.list[28].dt).format("ddd MM/DD/YYYY")
+    dayFiveDateEl.textContent = moment.unix(data.list[35].dt).format("ddd MM/DD/YYYY")
+
+    dayOneIconDisplayEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[7].weather[0].icon + ".png")
+    dayTwoIconDisplayEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[14].weather[0].icon + ".png")
+    dayThreeIconDisplayEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[21].weather[0].icon + ".png")
+    dayFourIconDisplayEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[28].weather[0].icon + ".png")
+    dayFiveIconDisplayEl.setAttribute("src", "http://openweathermap.org/img/w/" + data.list[35].weather[0].icon + ".png")
+
+    dayOneTmpEl.textContent = "Temp: " + Math.round(data.list[7].main.temp) + " ℉"
+    dayTwoTmpEl.textContent = "Temp: " + Math.round(data.list[14].main.temp) + " ℉"
+    dayThreeTmpEl.textContent = "Temp: " + Math.round(data.list[21].main.temp) + " ℉"
+    dayFourTmpEl.textContent = "Temp: " + Math.round(data.list[28].main.temp) + " ℉"
+    dayFiveTmpEl.textContent = "Temp: " + Math.round(data.list[35].main.temp) + " ℉"
+
+    dayOneWndEl.textContent = "Wind: " + Math.round(data.list[7].wind.speed) + " MPH"
+    dayTwoWndEl.textContent = "Wind: " + Math.round(data.list[14].wind.speed) + " MPH"
+    dayThreeWndEl.textContent = "Wind: " + Math.round(data.list[21].wind.speed) + " MPH"
+    dayFourWndEl.textContent = "Wind: " + Math.round(data.list[28].wind.speed) + " MPH"
+    dayFiveWndEl.textContent = "Wind: " + Math.round(data.list[35].wind.speed) + " MPH"
+
+    dayOneHumdityEl.textContent = "Humidity: " + data.list[7].main.humidity + " %"
+    dayTwoHumdityEl.textContent = "Humidity: " + data.list[14].main.humidity + " %"
+    dayThreeHumdityEl.textContent = "Humidity: " + data.list[21].main.humidity + " %"
+    dayFourHumdityEl.textContent = "Humidity: " + data.list[28].main.humidity + " %"
+    dayFiveHumdityEl.textContent = "Humidity: " + data.list[35].main.humidity + " %"
 
 }
 
